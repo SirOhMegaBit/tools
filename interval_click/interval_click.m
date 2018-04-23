0bf013c18f627c1f90daadc96e1e1d203a06d2f5
SirOhMegaBit <SirOhMegaBit@users.noreply.github.com>
2018-04-23 21:32:05 +1000

    Minor update to add comments, autoreleasepool

--- interval_click/interval_click.m
+++ interval_click/interval_click.m
@@ -1,9 +1,7 @@
-// Compile with: 
-// gcc -o click interval_click.m -framework ApplicationServices -framework Foundation
-//
 // Usage:
-// ./click -x pixels -y pixels -t seconds
-// At the given coordinates it will click and release.
+// ./<application> -x <x_pixels> -y <y_pixels> -t <seconds>
+// At the given coordinates it will click and release, after and every <seconds> seconds.
+// Except if <seconds>=0 it will only click the mouse once, then quit
 
 #import <Foundation/Foundation.h>
 #import <ApplicationServices/ApplicationServices.h>
@@ -22,27 +20,29 @@ void leftClickLocation(const CGPoint point, const unsigned int millisec) {
 }
 
 int main(int argc, char *argv[]) {
-  NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+   @autoreleasepool {
+      NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
 
-  // grabs command line arguments -x, -y and -t
-  unsigned int x = [args integerForKey:@"x"];
-  unsigned int y = [args integerForKey:@"y"];
-  unsigned int t = [args integerForKey:@"t"];
+      // grabs command line arguments -x, -y and -t
+      unsigned int x = [args integerForKey:@"x"];
+      unsigned int y = [args integerForKey:@"y"];
+      unsigned int t = [args integerForKey:@"t"];
 
-  // Define the point to be clicked
-  CGPoint point;
-  point.x = x;
-  point.y = y;
+      // Define the point to be clicked
+      CGPoint point;
+      point.x = x;
+      point.y = y;
 
-  // Click every t seconds
-  if (t > 0) {
-     while (true) {
-        sleep(t);
-        leftClickLocation(point, 100);
-     }
-  } else {
-     leftClickLocation(point, 100);
-  }
+      // Click every t seconds
+      if (t > 0) {
+         while (true) {
+            sleep(t);
+            leftClickLocation(point, 100);
+         }
+      } else {
+         leftClickLocation(point, 100);
+      }
+   }
 
-  return 0;
+   return 0;
 }
